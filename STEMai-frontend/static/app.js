9a0e04f0f142486e3bf6800a628cce78f2044e01
Krishna Moghe <krishnamoghe74@gmail.com>
2026-04-11 12:57:38 +0530

    Added nginx folder and made changes to remove the hardcoded links

--- STEMai-frontend/static/app.js
+++ STEMai-frontend/static/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04c1a399514b891f547336cf5be2d412fd17733d9aa9dbd49f478417552a1db9
-size 45633
+oid sha256:db731d81b38cfcb51e5c60a04539353b3cc4c7d9035b986411fd54e9b2aacf56
+size 45149
